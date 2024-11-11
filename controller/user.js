0da64796a8a368883babecab7057de49acbd8cde
vs45 <bringfirehjake88@gmail.com>
2024-11-11 15:04:29 +0100

    ignore the .env file

--- controller/user.js
+++ controller/user.js
@@ -1,12 +1,15 @@
 const User = require("../model/users");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-exports.getUsers = (req, res) => {
-  User.findAllUsers((err, users) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    return res.status(200).json(users);
-  });
+const crypto = require("crypto");
+require("dotenv").config({ path: __dirname + "/.env" });
+exports.getUsers = async(req, res) => {
+ try{
+const users=await User.find();
+return res.status(200).json(users)
+ }catch(err){
+ return res.status(500).json({message:err.message})
+ }
 };
 
 exports.postUser = async (req, res) => {
@@ -19,5 +22,42 @@ exports.postUser = async (req, res) => {
     return res.status(201).json(user);
   } catch (err) {
     return res.staus(500).json({ message: err });
+  }
+};
+
+exports.postLogin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email });
+  try {
+    console.log(`Inside, ${user}`);
+    if (user) {
+      const doMatch = await bcrypt.compare(password, user.password);
+      if (doMatch) {
+        const token = jwt.sign(
+          {
+            email: user.email,
+            userId: user._id.toString(),
+          },
+          process.env.JWT_SECRET,
+          { expiresIn: "8760h" }
+        );
+        // return sendJson(res, { token: token, userId: user._id });
+        return res.status(200).json({ token: token, userId: user._id });
+      } else {
+        return res
+          .status(403)
+          .json({ message: `Password ${password} is Wrong` });
+      }
+    } else {
+      //return sendApiError(res, `User with email ${email} not Found `);
+      return res
+        .status(403)
+        .json({ message: `User with email ${email} not Found ` });
+    }
+  } catch (err) {
+    // res.status(500).json({ message: "Could not logged In" });
+    return res
+      .status(500)
+      .json({ message: `Could not logged In:  ${err.message}` });
   }
 };
--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getUsers, postUser } = require("../controller/user");
+const { getUsers, postUser, postLogin } = require("../controller/user");
 
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.post("/users", postUser);
+router.post("/login", postLogin);
 module.exports = router;
